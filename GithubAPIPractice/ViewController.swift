d7de66c2f668f59cf239c5c627cb83e142ffcdda
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2022-02-12 14:50:16 +0900

    Initial Commit

---
+++ GithubAPIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPIPractice
+//
+//  Created by 三浦　一真 on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
